0c14b8f060f2261ebbdf034be7689fa895fb7c01
Robert Glenholmes <robert.glenholmes@gmail.com>
2023-04-06 23:00:59 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Tizen-nightscout-widget
+ Very simple widget that queries nightscout API to display blood glucose iin the form of the widget.
---
+++ widget/Nightscout/js/main.js
@@ -0,0 +1,33 @@
+
+window.onload = function() {
+  console.log('+[window.onload]');
+  // Do nothing now
+  var myapi = "https://YOURNIGHTSOCUT/api/v1/entries.json";
+	  
+  document.getElementById("update").addEventListener("click", updateCurrent);
+  
+  
+  
+  
+  function updateCurrent() {
+	    document.getElementById("update").textContent = "Updating...";
+	    setTimeout(function(){
+	  	  document.getElementById("update").textContent = "Update";
+	  	  return;
+	  	}, 5000);
+	    setTimeout(function(){
+	    	var xmlHttp = new XMLHttpRequest();
+		    xmlHttp.open( "GET", myapi, false ); // false for synchronous request
+		    xmlHttp.send( null );
+		    var obj = JSON.parse(xmlHttp.responseText);
+		    document.getElementById("current").textContent = String(Math.round(obj[0].sgv * 0.0555 * 10)/10);
+		    
+		   
+		    document.getElementById("update").textContent = "Update";
+		    
+		    document.getElementById("time").textContent =  new Date().toTimeString().split(" ")[0];
+		  	}, 100);
+	    
+	  }
+  
+};
